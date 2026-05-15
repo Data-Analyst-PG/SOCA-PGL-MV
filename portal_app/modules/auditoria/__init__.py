260ab1bfc72622d8af1a3f0f68b5fa3e59c53e18
Data-Analyst-PG <data.analyst@palosgarza.com>
2026-05-15 18:11:53 -0500

    Create __init__.py

---
+++ portal_app/modules/auditoria/__init__.py
@@ -0,0 +1,23 @@
+# portal_app/modules/auditoria/__init__.py
+from . import (
+    reporte_auxiliares,
+    rutas_frecuentes,
+    rentabilidad_clientes,
+    prorrateador,
+    lincoln_auditoria,
+    seguimiento_sac_ventas,
+    cartera_proveedores,
+    reporte_balanza_mensual,
+    admin_manager,
+)
+
+
+def reporteauxiliares_page():      reporte_auxiliares.render()
+def rutasfrecuentes_page():        rutas_frecuentes.render()
+def rentabilidadclientes_page():   rentabilidad_clientes.render()
+def prorrateador_page():           prorrateador.render()
+def lincolnauditoria_page():       lincoln_auditoria.render()
+def seguimientosacventas_page():   seguimiento_sac_ventas.render()
+def carteraproveedores_page():     cartera_proveedores.render()
+def reportebalanzamensual_page():  reporte_balanza_mensual.render()
+def admin_manager_page():          admin_manager.render()
