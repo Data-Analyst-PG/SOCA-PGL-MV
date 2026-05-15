b02a2b5100ba34c457877af25a423774ba8f12f8
Data-Analyst-PG <data.analyst@palosgarza.com>
2026-05-15 17:05:36 -0500

    Create __init__.py

---
+++ portal_app/ui/__init__.py
@@ -0,0 +1,11 @@
+from ui.theme import aplicar_tema, user_header
+from ui.components import (
+    page_banner,
+    section_header,
+    kpi_row,
+    module_card,
+    alert,
+    status_badge,
+    divider,
+    welcome_banner,
+)
