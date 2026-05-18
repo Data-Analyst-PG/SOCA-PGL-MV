c915086d60bab22810edd30474d2bb4cf1f22fa3
Data-Analyst-PG <data.analyst@palosgarza.com>
2026-05-18 16:31:29 -0500

    Update __init__.py

--- portal_app/modules/gestion_solicitudes/__init__.py
+++ portal_app/modules/gestion_solicitudes/__init__.py
@@ -1,2 +1,7 @@
-# portal_app/modules/gestion_solicitudes/__init__.py
-from .gestion_router import render
+from ui.components import page_banner, section_header, alert, divider
+from .gestion_router import render as page
+
+def render():
+    """Alias para que pg_gestion_solicitudes.py pueda llamar el módulo."""
+    from .gestion_router import render as _render
+    _render()
