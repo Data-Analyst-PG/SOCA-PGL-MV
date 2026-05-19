51aa0c931f4bde835e2421d628105bf2e99bc3e1
Data-Analyst-PG <data.analyst@palosgarza.com>
2026-05-19 17:29:15 -0500

    Update __init__.py

--- portal_app/modules/facturacion/__init__.py
+++ portal_app/modules/facturacion/__init__.py
@@ -1,1 +1,9 @@
-from modules.facturacion.facturacion import estado_cuenta_page
+# portal_app/modules/facturacion/__init__.py
+from .estado_cuenta import render as _estado_cuenta
+from .cargar_datos  import render as _cargar_datos
+
+def estado_cuenta_page():
+    _estado_cuenta()
+
+def cargar_datos_page():
+    _cargar_datos()
