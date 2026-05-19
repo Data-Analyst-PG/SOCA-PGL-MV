e1f7951e66bb00494d23e91b56ce6d13c5c691be
Data-Analyst-PG <data.analyst@palosgarza.com>
2026-05-19 15:25:09 -0500

    Refactor import statements for better readability

--- portal_app/modules/cotizadores/set_freight/__init__.py
+++ portal_app/modules/cotizadores/set_freight/__init__.py
@@ -1,2 +1,7 @@
-from ui.components import section_header, alert, divider
-from . import captura_rutas, consulta_ruta, gestion_rutas, simulador, cotizacion
+from . import (
+  captura_rutas, 
+  consulta_ruta, 
+  gestion_rutas, 
+  simulador, 
+  cotizacion
+)
