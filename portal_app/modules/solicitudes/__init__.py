fee62b9176ad1fa8f3649ad27a21cb6f345e4404
Data-Analyst-PG <data.analyst@palosgarza.com>
2026-07-20 15:57:31 -0500

    Delete render function from solicitudes module
    Remove the render function and its alias for solicitudes.

--- portal_app/modules/solicitudes/__init__.py
+++ portal_app/modules/solicitudes/__init__.py
@@ -1,6 +1,1 @@
-from .solicitudes_router import render as page
 
-def render():
-    """Alias para que pg_solicitudes.py pueda llamar solicitudes.render()"""
-    from .solicitudes_router import render as _render
-    _render()
